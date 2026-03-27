10bdd4f684ad5793d075a40fb98cb9e4f1866c0d
bbautista-2025051 <bbautista-2025051@kinal.edu.gt>
2026-03-26 22:31:23 -0600

    feat: se agrega nueva caracteristica al archivo

--- src/main/java/com/brayanbautista/kinalapp/repository/ProductoRepository.java
+++ src/main/java/com/brayanbautista/kinalapp/repository/ProductoRepository.java
@@ -2,9 +2,8 @@ package com.brayanbautista.kinalapp.repository;
 
 import com.brayanbautista.kinalapp.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface ProductoRepository extends JpaRepository<Producto, String> {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
     List<Producto> findByEstado(int estado);
 }
