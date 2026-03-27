25d360a804a21ae3389057af414e1a5023cbf21d
bbautista-2025051 <bbautista-2025051@kinal.edu.gt>
2026-03-26 21:41:50 -0600

    feat: se agrega el controlador de producto

---
+++ src/main/java/com/brayanbautista/kinalapp/controller/ProductoController.java
@@ -0,0 +1,10 @@
+package com.brayanbautista.kinalapp.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producto")
+public class ProductoController {
+}
