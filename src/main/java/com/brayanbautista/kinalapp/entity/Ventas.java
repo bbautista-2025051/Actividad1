b625617f0251f536c3d4823e2eef56c8e547dfbe
bbautista-2025051 <bbautista-2025051@kinal.edu.gt>
2026-03-26 18:49:48 -0600

    Refactor: se termina la entidad de Venta

--- src/main/java/com/brayanbautista/kinalapp/entity/Ventas.java
+++ src/main/java/com/brayanbautista/kinalapp/entity/Ventas.java
@@ -1,24 +1,90 @@
 package com.brayanbautista.kinalapp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
-@Table(name = "ventas")
-public class Ventas {
+@Table(name= "Ventas")
+public class Venta {
     @Id
-    @Column(name = "codigo_ventas")
-    private int CodigoVentas;
+    @Column(name = "codigo_venta")
+    private int CodigoVenta;
     @Column
-    private Date FechaVentas;
+    private LocalDate FechaVenta;
     @Column
-    private Float total;
+    private BigDecimal total;
     @Column
     private int Estado;
-    
+
+    @ManyToOne
+    @JoinColumn(name = "Clientes_dpi_cliente", referencedColumnName = "dpi_cliente")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "Usuarios_codigo_usuario", referencedColumnName = "codigo_usuario")
+    private Usuario usuario;
+
+    public Venta() {
+    }
+
+    public Venta(int codigoVenta, LocalDate fechaVenta, BigDecimal total, int estado, Cliente cliente, Usuario usuario) {
+        CodigoVenta = codigoVenta;
+        FechaVenta = fechaVenta;
+        this.total = total;
+        Estado = estado;
+        this.cliente = cliente;
+        this.usuario = usuario;
+    }
+
+    public int getCodigoVenta() {
+        return CodigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        CodigoVenta = codigoVenta;
+    }
+
+    public LocalDate getFechaVenta() {
+        return FechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        FechaVenta = fechaVenta;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(int estado) {
+        Estado = estado;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
