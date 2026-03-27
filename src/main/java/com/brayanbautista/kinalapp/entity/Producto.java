dba622a0df6f17ef01eebdfe76563dc208dc4341
bbautista-2025051 <bbautista-2025051@kinal.edu.gt>
2026-03-26 19:09:01 -0600

    feat:se sube la entidad de producto

---
+++ src/main/java/com/brayanbautista/kinalapp/entity/Producto.java
@@ -0,0 +1,76 @@
+package com.brayanbautista.kinalapp.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="Productos")
+public class Producto {
+    @Id
+    @Column(name="codigo_producto")
+    private int CodigoProducto;
+    @Column
+    private String NombreProducto;
+    @Column
+    private BigDecimal precio;
+    @Column
+    private int stock;
+    @Column
+    private int estado;
+
+    public Producto() {
+    }
+
+    public Producto(int codigoProducto, String nombreProducto, BigDecimal precio, int stock, int estado) {
+        CodigoProducto = codigoProducto;
+        NombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getCodigoProducto() {
+        return CodigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        CodigoProducto = codigoProducto;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        NombreProducto = nombreProducto;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}
