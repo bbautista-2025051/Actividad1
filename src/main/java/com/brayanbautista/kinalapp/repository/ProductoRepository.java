f93b165da363365187c4ec907735c4929245dcad
bbautista-2025051 <bbautista-2025051@kinal.edu.gt>
2026-03-26 19:15:57 -0600

    feat:se sube el repository de producto

---
+++ src/main/java/com/brayanbautista/kinalapp/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.brayanbautista.kinalapp.repository;
+
+import com.brayanbautista.kinalapp.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository<Producto, String> {
+    List<Producto> findByEstado(int estado);
+}
